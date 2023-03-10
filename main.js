f3a6b749bed5ef381c5a969d52b3c8ae30d8f0de
rassi0429 <taitokokoa+rassi@gmail.com>
2023-03-10 14:16:52 +0900

    feat: speed query

--- main.js
+++ main.js
@@ -10,17 +10,22 @@ app.listen(3000, () => console.log("ok"))
 
 const client = new Client(`http://${process.env.VOICE_HOST || "localhost"}:50021`)
 
+const apiEndpoint = `http://${process.env.VOICE_HOST || "localhost"}:50021`
+
 const processingMap = new Map()
 const waithingMap = new Map()
 
 app.get("/speakers", async (req, res) => {
-    const { data } = await axios.get(`http://${process.env.VOICE_HOST || "localhost"}:50021/speakers`)
+    const { data } = await axios.get(`${apiEndpoint}/speakers`)
     res.send(req.query.emap === "true" ? j2e(emap) : data)
 })
 
 app.get("/", async (req, res) => {
     const text = req.query.text
     const speakerId = req.query.speaker || 0
+    let speed = Number(req.query.speed) || 1
+
+    if(speed <= 0.1) speed = 1
     
     if(!text) {
         return res.sendFile(path.join(__dirname, `./fusei.wav`))
@@ -33,7 +38,7 @@ app.get("/", async (req, res) => {
     }
     if (!processingMap.has(hash)) {
         processingMap.set(hash, true)
-        await createVoice(text, hash, speakerId)
+        await createVoice(text, hash, speakerId, speed)
         const waithing = waithingMap.get(hash)
         if(waithing) {
             waithing.forEach((wres) => wres.sendFile(path.join(__dirname, `./wav/${hash}`)))
@@ -50,11 +55,11 @@ app.get("/", async (req, res) => {
     }
 })
 
-const createVoice = async (text, hash, speakerId) => {
+const createVoice = async (text, hash, speakerId, speed) => {
     console.log("cq")
     const query = await client.query.createQuery(speakerId, text)
     console.log("cv")
-    const voice = await client.voice.createVoice(speakerId, query)
+    const voice = await client.voice.createVoice(speakerId, {...query, speedScale: speed})
     console.log("cv ok")
     const buf = Buffer.from(voice)
     await fs.writeFile(`./wav/${hash}`, buf)
